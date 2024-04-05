db9458e3c719ab96f1457e338d6c4f8ab49be8b6
Lucas Gabriel <Lucas Gabriel>
2024-04-05 10:39:29 -0300

    add: insert logo image and update styles

--- src/pages/Login/style.js
+++ src/pages/Login/style.js
@@ -4,71 +4,72 @@ import font from '../../styles/fonts';
 const fontFamily = font.FONTS;
 
 export const MainContainer = styled.main`
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100vh;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100vh;
 `;
 
 export const Content = styled.div`
-    position: relative;
-    margin-top: 90px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    font-family: ${fontFamily.FONT_100};
-    font-size: 18px;
-    text-align: center;
+  position: relative;
+  margin-top: 90px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-family: ${fontFamily.FONT_100};
+  font-size: 18px;
+  text-align: center;
 
-    .subtitle {
-        font-size: 20px;
-    }
+  .subtitle {
+    font-size: 20px;
+    margin-top: 20px;
+  }
 `;
 
 export const FormData = styled.form`
-    margin-top: 20px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    flex-direction: column;
-    padding: 22px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+
+  padding-top: 40px;
 `;
 
 export const InputBox = styled.div`
-    flex: 1 1;
-    width: 24rem;
-    margin: 20px 0;
-    border-radius: 1rem;
+  flex: 1 1;
+  width: 24rem;
+  margin: 10px 0;
+  border-radius: 1rem;
 `;
 
 export const Input = styled.input`
-    width: 100%;
-    padding: 14px 22px;
-    background-color: #F4F4F4;
-    border-radius: 1rem;
+  width: 100%;
+  padding: 14px 22px;
+  background-color: #f4f4f4;
+  border-radius: 1rem;
 `;
 
 export const InputButton = styled.div`
-    flex: 1 1;
-    width: 100%;
-    margin: 12px 0;
-    border-radius: 6px;
-    display: flex;
-    justify-content: center;
+  flex: 1 1;
+  width: 100%;
+  margin: 12px 0;
+  border-radius: 6px;
+  display: flex;
+  justify-content: center;
 `;
 
 export const Button = styled.button`
-    align-items: center;
-        padding: 10px;
-        background-color: green;
-        margin-top: 2px; // Espaçamento entre o texto e o botão
-        border-radius: 15px;
-        background-color: #00FF59;
-        color: rgb(0, 0, 0);
-        font-weight: bold;
-        cursor: pointer;
+  align-items: center;
+  padding: 10px;
+  background-color: green;
+  margin-top: 2px; // Espaçamento entre o texto e o botão
+  border-radius: 15px;
+  background-color: #00ff59;
+  color: rgb(0, 0, 0);
+  font-weight: bold;
+  cursor: pointer;
 
-        font-family: 'Montserrat', sans-serif;
+  font-family: 'Montserrat', sans-serif;
 `;
